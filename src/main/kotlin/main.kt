0d8838080dfe07194781ea16cb7285e63c6ab2df
braiskiskos <bmartinezparedes@danielcastelao.org>
2021-06-06 18:16:02 +0200

    Ejercicio2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,8 @@
-import java.util.*
+import kotlin.random.Random
 
 fun main(args: Array<String>) {
     println("Algo aprendí")
 
-    //Ejercicio1
-    println(Ejercicio1(10).tablaSiete.contentToString())
+    //Ejercicio2
+    println(Ejercicio1(10){ Random.nextInt(50,250)}.tablaSiete.contentToString())
 }
